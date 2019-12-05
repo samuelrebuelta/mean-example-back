619925086d97707e7695ac6f2bbd5e52137d78ce
samuelrebuelta <31918772+samuelrebuelta@users.noreply.github.com>
2019-12-05 14:47:39 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Controll-Allow-Origin', '*');
-    res.setHeader('Access-Controll-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+const app = express();
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+    response.setHeader('Access-Controll-Allow-Origin', '*');
+    response.setHeader('Access-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
 });
 
-app.use('/fetch/example', (req, res, next) => {
-    const cars = ['Audi', 'Bmw', 'Mercedes', 'Volkswagen'];
-    return res.status(200).json({
-        message: 'Fetch example works',
-        data: cars
+app.get('/brands', (request, response, next) => {
+    const brands = ['Audi', 'Bmw', 'Mercedes', 'Volkswagen'];
+    return response.status(200).json({
+        message: 'Data fetched succesfully',
+        data: brands
     });
+});
+
+app.post('/brands', (request, response, next) => {
+    const post = request.body;
+    console.log(post);
+    response.status(201).json({
+        message: 'Post added succesfully',
+        data: null
+    })
 });
 
 module.exports = app;
